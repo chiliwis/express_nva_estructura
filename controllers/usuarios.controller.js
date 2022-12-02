268b006fdb7996480019e4990cdfef2c5f7479d7
Cristina SG <cristinasanchezg@gmail.com>
2022-12-01 21:55:48 -0600

    se completo el GET

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,22 +1,37 @@
 const { response, request } = require('express');
 const Usuario = require('../models/Usuario.model');
 
-const usuariosGet = (req = request, res = response) => {
-    
-    res.status(200).json({
-        msg: 'GET API - controlador'
-    })
-};
+const usuariosGet = async (req = request, res = response) => {
+    try {
+        const usuarios = await Usuario.find();
+        res.status(200).json({
+            msg: 'GET API - controlador',
+            data: usuarios
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error al crear el usuario',
+            details: error.message
+        });
+    }
+}
 
 const usuariosPost = async (req = request, res = response) => {
-    const body = req.body;
-    const usuario = new Usuario(body);
-    await usuario.save()
-    res.status(200).json({
-        msg: 'POST API - controlador',
-        post: body,
-        usuario: usuario
-    });
+    try {
+        const body = req.body;
+        const usuario = new Usuario(body);
+        await usuario.save()
+        res.status(200).json({
+            msg: 'POST API - controlador',
+            post: body,
+            usuario: usuario
+        });
+    } catch (error) {
+        res.status(400).json({
+            msg: 'se detecto un error',
+            detalle: error.message
+        });
+    }
 };
 
 
